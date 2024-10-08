69ad209d699965f78b88baf19dfd3e6cef9ba0a3
f.areias <nando.calheirosx@gmail.com>
2024-10-08 06:41:09 -0300

    Updating writer

--- src/workers/src/main/java/com/fernando/payments/processor/workers/writers/JdbcCreditPaymentProcessorWriterConfig.java
+++ src/workers/src/main/java/com/fernando/payments/processor/workers/writers/JdbcCreditPaymentProcessorWriterConfig.java
@@ -21,46 +21,36 @@ public class JdbcCreditPaymentProcessorWriterConfig {
     @Bean
     @Transactional
     public ItemWriter<CreditCardPayment> jdbcCursorWriter() {
+
         return new ItemWriter<CreditCardPayment>() {
             @Override
             public void write(Chunk<? extends CreditCardPayment> items) throws Exception {
                 for (CreditCardPayment payment : items) {
-                    String updatePaymentSql = "UPDATE db_credit_card_payments SET amount = ?, cardcvc = ?, card_expiry = ?, card_number = ?, recipient_document = ?, status = ? WHERE id = ?";
+                    String updatePaymentSql = "UPDATE db_credit_card_payments SET status = ? WHERE id = ?";
 
                     jdbcTemplate.update(updatePaymentSql,
-                            payment.getAmount(),
-                            payment.getCardCVC(),
-                            payment.getCardExpiry(),
-                            payment.getCardNumber(),
-                            payment.getRecipientDocument(),
                             payment.getStatus().getCode(),
                             payment.getId()
                     );
 
 
 
-                    try {
-                        String insertProcessorSql = "INSERT INTO db_processor (id, created_at, response, status, processors) VALUES (DEFAULT, ?, ?, ?, ?)";
+                    String insertProcessorSql = "INSERT INTO db_processor (id, created_at, response, status, processors) VALUES (DEFAULT, ?, ?, ?, ?)";
 
-                        var firstProcessor = payment.getProcessors().stream().findFirst()
-                                .orElseThrow(() -> new IllegalArgumentException("Nenhum processor encontrado"));
+                    var firstProcessor = payment.getProcessors().stream().findFirst()
+                            .orElseThrow(() -> new IllegalArgumentException("Nenhum processor encontrado"));
 
-                        Instant createdAt = firstProcessor.getCreatedAt();
-                        String response = firstProcessor.getResponse();
-                        int status = firstProcessor.getStatus().getCode();
-                        var paymentId = payment.getId();
+                    Timestamp createdAt = Timestamp.from(firstProcessor.getCreatedAt());
+                    String response = firstProcessor.getResponse();
+                    int status = firstProcessor.getStatus().getCode();
+                    Long paymentId = payment.getId();
 
-                        Timestamp timestamp = Timestamp.from(createdAt);
-
-                        jdbcTemplate.update("INSERT INTO db_processor (id, created_at, response, status, processors) VALUES (?, ?, ?, ?, ?)",
-                                UUID.randomUUID().toString(), Timestamp.from(createdAt), response, status, paymentId);
-
-                    } catch (IllegalArgumentException e) {
-                        System.err.println(e.getMessage());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
+                    jdbcTemplate.update("INSERT INTO db_processor (created_at, response, status, processors) VALUES (?, ?, ?, ?)",
+                            createdAt,
+                            response,
+                            status,
+                            paymentId
+                    );
                 }
             }
         };
